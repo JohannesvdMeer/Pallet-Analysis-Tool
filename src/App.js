cca3beb24ff8603bf456f0316615394408c99cf8
JohannesvdMeer <123605618+JohannesvdMeer@users.noreply.github.com>
2025-03-12 22:20:09 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PalletAnalysisTool from './PalletAnalysisTool';
+
+function App() {
+  return (
+    <div className="App">
+      <PalletAnalysisTool />
+    </div>
+  );
+}
+
+export default App;
